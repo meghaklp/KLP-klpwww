639920353cd2ca20b67c8e180431bd0cb5932b06
meghaklp <megha@klp.org.in>
2011-07-05 16:07:08 +0530

    Added Javascript to count characters on comment field

--- js/shareyourstory.js
+++ js/shareyourstory.js
@@ -23,6 +23,13 @@ function isNumeric(str)
   return true;
 }
 
+function textCounter(field,cntfield,maxlimit) {
+  if (field.value.length > maxlimit) // if too long...trim it!
+    field.value = field.value.substring(0, maxlimit);
+  // otherwise, update 'characters left' counter
+  else
+    cntfield.value = maxlimit - field.value.length;
+}
 
 function initialise()
 {
